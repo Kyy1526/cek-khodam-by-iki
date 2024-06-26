22b57c852bb6b600e725d82ec2641ee8e74c9971
Kyy1526 <156793860+Kyy1526@users.noreply.github.com>
2024-06-26 10:26:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function checkKhodam() {
+    const name = document.getElementById('nameInput');
+    const resultDiv = document.getElementById('result');
+
+    if (name === "") {
+        resultDiv.innerHTML = "<P style='color: red;'>Nama tidak boleh kosong</P>";
+        return;
+    }
+
+    const khodamNames = [
+        "KAI ZEUS",
+        "INCES KALA",
+        "NAGA CINA"
+    ];
+
+    const khodamPresent = Math.random() > 0.5;
+
+    if (khodamPresent) {
+        const randomIndex = [Math.floor(Math.random() * khodamNames.length)];
+        const khodamTerpilih = khodamNames[randomIndex]
+        resultDiv.innerHTML =`<p style='color: green;'> Anda memiliki khodam ${khodamTerpilih} didalam diri anda  </p>`;
+    } else {
+        resultDiv.innerHTML = `<p style='color: red;'> maaf, nyawa balum MENDEPO makanya kdd bisi khodam. </p>`;
+    }
+}
